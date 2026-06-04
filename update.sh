cb81ebdee44e81e06c75f9148377100b21ed1ae1
zKhadiri <zakariya-khadiri@hotmail.fr>
2026-06-04 21:34:27 +0100

    Add support for python3.14

--- update.sh
+++ update.sh
@@ -15,7 +15,7 @@ RESET='\033[0m'
 IPK=""
 ARCH=""
 PY_VER=""
-VERSION="1.3-r02"
+VERSION="1.3-r03"
 BASE_URL="https://raw.githubusercontent.com/zKhadiri/MultiStalkerPro-releases/refs/heads/main"
 
 REQUIRED_PYTHON_DEPS=(
