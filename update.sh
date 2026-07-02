a6cc05cf7a165822dda425a5ba0f3452663a8bea
zKhadiri <zakariya-khadiri@hotmail.fr>
2026-07-02 08:46:42 +0100

    Bug fixes

--- update.sh
+++ update.sh
@@ -15,7 +15,7 @@ RESET='\033[0m'
 IPK=""
 ARCH=""
 PY_VER=""
-VERSION="1.3-r08"
+VERSION="1.3-r09"
 BASE_URL="https://raw.githubusercontent.com/zKhadiri/MultiStalkerPro-releases/refs/heads/main"
 
 REQUIRED_PYTHON_DEPS=(
